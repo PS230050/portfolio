5dd1fdf9a1049f06ff08289be8914ba81f489d3a
iwandk <iwandkinderen1@gmail.com>
2023-10-30 14:03:51 +0100

    init

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+function vergroot(){
+    if (document.getElementById("snapcodeimg").style.width==="50px"){
+        document.getElementById("snapcodeimg").style.width="250px";
+        document.getElementById("snapcodeimg").style.height="250px";
+    }
+    else{
+        document.getElementById("snapcodeimg").style.width="50px";
+        document.getElementById("snapcodeimg").style.height="50px";
+    }
+}
